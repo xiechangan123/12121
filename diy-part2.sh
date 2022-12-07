8c61e73fe40ad04fcc620633e8d78ed874929f08
xiechangan123 <xiechangan123@139.com>
2022-12-07 16:26:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # Modify default TimeZone
-#sed -i 's/UTC/UTC+8/g' package/base-files/files/bin/config_generate
+#sed -i 's/UTC/Asia/Shanghai/g' package/base-files/files/bin/config_generate
 # Modify default Password
 sed -i 's/root:::0:99999:7:::/root:$1$0MHbqDVw$FMCswXfbF5apRBrTWoVfA0:19333:0:99999:7:::/g' package/base-files/files/etc/shadow
