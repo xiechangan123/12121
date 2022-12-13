69be6a63e140e50654ee4f6bf88c3db1c88c5fa6
xiechangan123 <xiechangan123@139.com>
2022-12-13 19:39:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,14 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Modify China TimeZone
-uci set system.@system[0].timezone=CST-8
-uci set system.@system[0].zonename=Asia/Shanghai
-uci commit system
-
 # Modify default password
 sed -i 's/root:::0:99999:7:::/root:$1$0MHbqDVw$FMCswXfbF5apRBrTWoVfA0:19333:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-# Modify disable dnsmasq from listening on pppoe-wan
-uci add_list dhcp.@dnsmasq[0].notinterface='pppoe-wan'
-uci commit dhcp
