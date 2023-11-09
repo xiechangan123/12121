6ecb27dad3352ca5055cd4c9661828a2408b949d
xiechangan123 <xiechangan123@139.com>
2023-11-10 01:03:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 
 # Add packages
 git clone https://github.com/xiechangan123/homebridger package/homebridger
-git clone https://github.com/zfl9/chinadns-ng package/chinadns-ng
+git clone https://github.com/xiechangan123/openwrt-chinadns-ng package/openwrt-chinadns-ng
