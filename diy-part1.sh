73f3cba35e2b60066cf2677de3847e6314f59e08
xiechangan123 <xiechangan123@139.com>
2024-09-19 23:11:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add packages
-git clone https://github.com/xiechangan123/helloworld package/helloworld
+#git clone https://github.com/xiechangan123/helloworld package/helloworld
